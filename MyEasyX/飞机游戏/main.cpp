c32cb79e203ac5aa2340d0a409d83c2a321f85aa
xuqiulin <1161851688@qq.com>
2020-03-09 08:10:55 +0800

    小飞机

---
+++ MyEasyX/飞机游戏/main.cpp
@@ -0,0 +1,44 @@
+#include <graphics.h>//引用EasyX图形库
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+#define WIDTH 40 //窗口宽度
+#define HIGH 30 //窗口高度
+
+int main() {
+	//小球坐标，初始化
+	int ball_x = WIDTH / 2;
+	int ball_y = HIGH / 2;
+	//小球速度
+	int ball_vx = 1;
+	int ball_vy = 1;
+
+	int x = 0;
+	int y = 0;
+	while (1) {
+		system("cls");
+
+		for (y = 0; y < ball_y; y++) {
+			printf("\n");
+		}
+		for (x = 0; x < ball_x; x++) {
+			printf(" ");
+		}
+		printf("0\n");
+
+
+		Sleep(3);
+		//ball_x += ball_vx;
+		//ball_y += ball_vy;
+
+		//if (ball_x <= 0 || ball_x >= WIDTH) {
+		//	ball_vx = -ball_vx;
+		//}
+		//if (ball_y <= 0 || ball_y >= HIGH) {
+		//	ball_vy = -ball_vy;
+		//}
+	}
+
+	return 0;
+}
