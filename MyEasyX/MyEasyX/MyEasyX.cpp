3d801880d05b676cd8bdd098505635bee7c51172
xuqiulin <1161851688@qq.com>
2020-03-29 23:29:11 +0800

    添加音效

--- MyEasyX/MyEasyX/MyEasyX.cpp
+++ MyEasyX/MyEasyX/MyEasyX.cpp
@@ -1,7 +1,36 @@
+#include <conio.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <graphics.h>
+
+#pragma comment(lib, "Winmm.lib")
+
+
 
 int main() {
-	puts("爱白菜的小昆虫");
+	//mciSendString(TEXT("open ./music/bullet.mp3 alias bullet"), NULL, 0, NULL);//打开音乐
+	//mciSendString(TEXT("play bullet"), NULL, 0, NULL);//仅播放一次
+
+
+	while (1) {
+
+		TCHAR szStatus[80];
+		mciSendString(TEXT("status bullet mode"), szStatus, 80, 0);
+
+
+		if (lstrcmp(szStatus, TEXT("")) == 0 ||
+			lstrcmp(szStatus, TEXT("stopped")) == 0
+			)
+		{
+			mciSendString(TEXT("close bullet"), NULL, 0, NULL);//先把之前的音乐关了
+			mciSendString(TEXT("open ./music/bullet.mp3 alias bullet"), NULL, 0, NULL);//打开音乐
+			mciSendString(TEXT("play bullet"), NULL, 0, NULL);//仅播放一次
+			wprintf(TEXT("11111111"));
+		}
+		wprintf(TEXT("%s\n"), szStatus);
+		Sleep(30);
+	}
 	getchar();
 	return 0;
 }
