1d875f609beea5bae674bb96f06abe93291f12e6
xuqiulin <1161851688@qq.com>
2020-04-06 22:11:02 +0800

    显示时钟数字

--- MyEasyX/俄罗斯方块/main.cpp
+++ MyEasyX/俄罗斯方块/main.cpp
@@ -1,29 +1,174 @@
-#include <stdio.h>
+
 #include <conio.h>
-#include <graphics.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
 
-#define Width 480
-#define High 480
+#pragma comment(lib, "Winmm.lib")
 
-#define Size 33
+#define High 20//游戏画面尺寸
+#define Width 10
+#define EnemyNum 5//敌机数量
 
-IMAGE img;
+//全局变量
+int field[High][Width] = { 0 };
 
+struct Point
+{
+	int x, y;
+} a[4], b[4];
 
+int figures[7][4] =
+{
+	1,3,5,7, // I
+	2,4,5,7, // Z
+	3,5,4,6, // S
+	3,5,4,7, // T
+	2,3,5,7, // L
+	3,5,7,6, // J
+	2,3,4,5, // O
+};
 
+int score;//游戏得分
 
-int main() {
-	loadimage(&img, _T("./image/fangkuai.png"));
+void gotoxy(int x, int y) {
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(handle, pos);
+}
 
-	initgraph(Width, High);
+//隐藏光标
+void HideCursor() {
+	CONSOLE_CURSOR_INFO cursor_info = { 1, 0 };//第二个值为0,表示隐藏光标
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+}
 
-	putimage(0, 0, &img);
+//数据初始化
+void startup() {
+	srand((unsigned int)time(NULL));//设置随机种子
+	HideCursor();//隐藏光标
 
-	for (int i = 0; i < 6; i++) {
-		putimage(50, 50+Size*i, Size, Size, &img, i*Size, 0);
+	int i, j, n;
+	n = rand() % 7;
+	for (i = 0; i < 4; i++)
+	{
+		a[i].x = figures[n][i] % 2;
+		a[i].y = figures[n][i] / 2;
 	}
 
-	_getch();
-	closegraph();
+	score = 0;
+}
+//显示画面
+void show() {
+	int i, j;
+	//system("cls");//清屏
+	gotoxy(0, 0);//光标移动到左上角，重画图片
+
+	for (i = 0; i < High; i++) {
+		for (j = 0; j < Width; j++) {
+			if (field[i][j] == 1) {
+				printf("*");//输出方块
+			}
+			else {
+				printf(" ");//输出
+			}
+		}
+		printf("#\n");//输出右边墙
+	}
+	// 输出下边墙
+	for (j = 0; j < Width; j++) {
+		printf("#");
+	}
+	printf("#\n");
+	printf("得分：%d\n", score);
+}
+//与用户输入无关的更新
+void updateWithoutInput() {
+	//int i, j, k;
+	////子弹上跑
+	//for (i = 0; i < High; i++) {
+	//	for (j = 0; j < Width; j++) {
+	//		if (canvas[i][j] == 2) {
+	//			//子弹击中敌机
+	//			for (k = 0; k < EnemyNum; k++) {
+	//				if (i == enemy_x[k] && j == enemy_y[k]) {
+	//					score++;
+
+	//					canvas[enemy_x[k]][enemy_y[k]] = 0;
+	//					enemy_x[k] = 0;
+	//					enemy_y[k] = rand() % Width;
+	//					canvas[enemy_x[k]][enemy_y[k]] = 3;
+	//				}
+	//			}
+
+	//			canvas[i][j] = 0;
+	//			if (i > 0) {
+	//				canvas[i - 1][j] = 2;
+	//			}
+	//		}
+	//	}
+	//}
+
+	//for (k = 0; k < EnemyNum; k++) {
+	//	if (enemy_x[k] + 1 >= High) {
+	//		canvas[enemy_x[k]][enemy_y[k]] = 0;
+	//		enemy_x[k] = 0;
+	//		enemy_y[k] = rand() % Width;
+	//		canvas[enemy_x[k]][enemy_y[k]] = 3;
+	//		score--;
+	//	}
+	//}
+	//static int speed = 0;
+	//if (speed < 10) {
+	//	speed++;
+	//}
+	//else {
+	//	for (k = 0; k < EnemyNum; k++) {
+	//		//敌机下落
+	//		canvas[enemy_x[k]][enemy_y[k]] = 0;
+	//		enemy_x[k]++;
+	//		canvas[enemy_x[k]][enemy_y[k]] = 3;
+	//		speed = 0;
+	//	}
+	//}
+}
+//与用户输入有关的更新
+void updateWithInput() {
+	//if (GetAsyncKeyState(0x41) & 0x8000 && position_y - 1 >= 0) { // a 左移
+	//	canvas[position_x][position_y] = 0;
+	//	position_y--;
+	//	canvas[position_x][position_y] = 1;
+	//}
+	//if (GetAsyncKeyState(0x44) & 0x8000 && position_y + 1 < Width) { // d 右移
+	//	canvas[position_x][position_y] = 0;
+	//	position_y++;
+	//	canvas[position_x][position_y] = 1;
+	//}
+	//if (GetAsyncKeyState(0x57) & 0x8000 && position_x - 1 >= 0) { // w 上移
+	//	canvas[position_x][position_y] = 0;
+	//	position_x--;
+	//	canvas[position_x][position_y] = 1;
+	//}
+	//if (GetAsyncKeyState(0x53) & 0x8000 && position_x + 1 < High) { // s 下移
+	//	canvas[position_x][position_y] = 0;
+	//	position_x++;
+	//	canvas[position_x][position_y] = 1;
+	//}
+	//if (GetAsyncKeyState(' ') & 0x8000) { // 空格 发射子弹
+	//	canvas[position_x - 1][position_y] = 2;
+	//}
+}
+
+int main() {
+	startup();//数据初始化
+	while (1) {//游戏循环执行
+		show();//显示画面
+		updateWithoutInput();//于用户输入无关的更新
+		updateWithInput();//与用户输入有关的更新
+	}
+
 	return 0;
 }
--- MyEasyX/时钟/main.cpp
+++ MyEasyX/时钟/main.cpp
@@ -1,23 +1,194 @@
-
-#include <graphics.h>
 #include <conio.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
 
+#pragma comment(lib, "Winmm.lib")
 
-#define High 480
-#define Width 640
+#define High 7//游戏画面尺寸
+#define Width 20
 
-int main() {
+int frame[High][Width];
+int pos[10] = { 0, 4, 8, 12 };// 显示数字的位置
 
+int num[12][7][4] = {
+	{
+		{0, 1, 1, 0}, // 0
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 0, 0, 0},
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 0, 0, 0}, // 1
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+	},{
+		{0, 1, 1, 0},// 2
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 1, 1, 0},
+		{1, 0, 0, 0},
+		{1, 0, 0, 0},
+		{0, 1, 1, 0},
+	},{
+		{0, 1, 1, 0},// 3
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 1, 1, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 0, 0, 0},// 4
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+	},{
+		{0, 1, 1, 0},// 5
+		{1, 0, 0, 0},
+		{1, 0, 0, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 1, 1, 0},// 6
+		{1, 0, 0, 0},
+		{1, 0, 0, 0},
+		{0, 1, 1, 0},
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 1, 1, 0},// 7
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 0, 0, 0},
+	},{
+		{0, 1, 1, 0},// 8
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 1, 1, 0},// 9
+		{1, 0, 0, 1},
+		{1, 0, 0, 1},
+		{0, 1, 1, 0},
+		{0, 0, 0, 1},
+		{0, 0, 0, 1},
+		{0, 1, 1, 0},
+	},{
+		{0, 0, 0, 0},// -
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+	},{
+		{0, 0, 0, 0},// :
+		{0, 1, 1, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+	},
+};
+
+//全局变量
+void gotoxy(int x, int y) {
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(handle, pos);
+}
+
+//隐藏光标
+void HideCursor() {
+	CONSOLE_CURSOR_INFO cursor_info = { 1, 0 };//第二个值为0,表示隐藏光标
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+}
+
+//数据初始化
+void startup() {
+	srand((unsigned int)time(NULL));//设置随机种子
+	HideCursor();//隐藏光标
+
+}
+//显示画面
+void show() {
+	int i, j;
+	//system("cls");//清屏
+	gotoxy(0, 0);//光标移动到左上角，重画图片
+
+	for (i = 0; i < High; i++) {
+		for (j = 0; j < Width; j++) {
+			if (frame[i][j] == 1) {
+				printf("*");//输出方块
+			}
+			else {
+				printf(" ");//输出
+			}
+		}
+		printf("#\n");//输出右边墙
+	}
+	// 输出下边墙
+	for (j = 0; j < Width; j++) {
+		printf("#");
+	}
+	printf("#\n");
+}
+//与用户输入无关的更新
+void updateWithoutInput() {
+	static int k = 0;
+
+	int i, j, p;
+	for (p = 0; p < 16; p += 4) {
+		for (i = 0; i < 7; i++) {
+			for (j = 0; j < 4; j++) {
+				frame[i][j + p] = num[k][i][j];
+			}
+		}
+	}
+
+	k++;
+	if (k >= 12) {
+		k = 0;
+	}
+	Sleep(500);
+}
+//与用户输入有关的更新
+void updateWithInput() {
 	SYSTEMTIME st;
-
-	initgraph(Width, High);
-
 	//获取当前系统时间
 	GetLocalTime(&st);
+}
 
-
-	_getch();
-	closegraph();
+int main() {
+	startup();//数据初始化
+	while (1) {//游戏循环执行
+		show();//显示画面
+		updateWithoutInput();//于用户输入无关的更新
+		updateWithInput();//与用户输入有关的更新
+	}
 
 	return 0;
 }
