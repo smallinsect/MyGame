04f57f192cb674806b0a092ccc654d561996ff18
xuqiulin <1161851688@qq.com>
2020-04-13 23:18:01 +0800

    飞机游戏增加附机

--- MyEasyX/飞机游戏1.0/main.cpp
+++ MyEasyX/飞机游戏1.0/main.cpp
@@ -7,8 +7,8 @@
 
 #pragma comment(lib, "Winmm.lib")
 
-#define High 20//游戏画面尺寸
-#define Width 50
+#define High 26//游戏画面尺寸
+#define Width 40
 #define EnemyNum 5//敌机数量
 
 //全局变量
@@ -20,6 +20,7 @@ int canvas[High][Width] = { 0 };
 
 
 int position_x, position_y;//飞机坐标位置
+int hero_x[2], hero_y[2];// 飞机的附机
 int enemy_x[EnemyNum], enemy_y[EnemyNum];//敌机坐标
 int score;//游戏得分
 
@@ -45,7 +46,15 @@ void startup() {
 	position_x = High / 2;//飞机坐标位置
 	position_y = Width / 2;
 
+	hero_x[0] = position_x;// 左附机
+	hero_y[0] = position_y - 1;
+
+	hero_x[1] = position_x;// 右附机
+	hero_y[1] = position_y + 1;
+
 	canvas[position_x][position_y] = 1;
+	canvas[hero_x[0]][hero_y[0]] = 4;
+	canvas[hero_x[1]][hero_y[1]] = 4;
 
 	int i;
 	// 初始化，随机敌机位置。
@@ -77,6 +86,12 @@ void show() {
 			else if (canvas[i][j] == 3) {
 				printf("@");//输出输出子弹@
 			}
+			else if (canvas[i][j] == 4) { // 输出附机
+				printf("+");
+			}
+			else if (canvas[i][j] == 5) { // 输出附机子弹
+				printf("^");
+			}
 			else {
 				printf(" ");//输出
 			}
@@ -96,7 +111,8 @@ void updateWithoutInput() {
 	//子弹上跑
 	for (i = 0; i < High; i++) {
 		for (j = 0; j < Width; j++) {
-			if (canvas[i][j] == 2) {
+			if (canvas[i][j] == 2 || canvas[i][j] == 5) {
+				int bullet = canvas[i][j];
 				//子弹击中敌机
 				for (k = 0; k < EnemyNum; k++) {
 					if (i == enemy_x[k] && j == enemy_y[k]) {
@@ -111,7 +127,7 @@ void updateWithoutInput() {
 
 				canvas[i][j] = 0;
 				if (i > 0) {
-					canvas[i - 1][j] = 2;
+					canvas[i - 1][j] = bullet;
 				}
 			}
 		}
@@ -143,27 +159,73 @@ void updateWithoutInput() {
 //与用户输入有关的更新
 void updateWithInput() {
 	if (GetAsyncKeyState(0x41) & 0x8000 && position_y - 1 >= 0) { // a 左移
+		canvas[hero_x[0]][hero_y[0]] = 0;
+		canvas[hero_x[1]][hero_y[1]] = 0;
 		canvas[position_x][position_y] = 0;
+
 		position_y--;
+		hero_x[0] = position_x;// 左附机
+		hero_y[0] = position_y - 1;
+
+		hero_x[1] = position_x;// 右附机
+		hero_y[1] = position_y + 1;
+
 		canvas[position_x][position_y] = 1;
+		canvas[hero_x[0]][hero_y[0]] = 4;
+		canvas[hero_x[1]][hero_y[1]] = 4;
 	}
 	if (GetAsyncKeyState(0x44) & 0x8000 && position_y + 1 < Width) { // d 右移
+		canvas[hero_x[0]][hero_y[0]] = 0;
+		canvas[hero_x[1]][hero_y[1]] = 0;
 		canvas[position_x][position_y] = 0;
+
 		position_y++;
+		hero_x[0] = position_x;// 左附机
+		hero_y[0] = position_y - 1;
+
+		hero_x[1] = position_x;// 右附机
+		hero_y[1] = position_y + 1;
+
 		canvas[position_x][position_y] = 1;
+		canvas[hero_x[0]][hero_y[0]] = 4;
+		canvas[hero_x[1]][hero_y[1]] = 4;
 	}
 	if (GetAsyncKeyState(0x57) & 0x8000 && position_x - 1 >= 0) { // w 上移
+		canvas[hero_x[0]][hero_y[0]] = 0;
+		canvas[hero_x[1]][hero_y[1]] = 0;
 		canvas[position_x][position_y] = 0;
+
 		position_x--;
+		hero_x[0] = position_x;// 左附机
+		hero_y[0] = position_y - 1;
+
+		hero_x[1] = position_x;// 右附机
+		hero_y[1] = position_y + 1;
+
 		canvas[position_x][position_y] = 1;
+		canvas[hero_x[0]][hero_y[0]] = 4;
+		canvas[hero_x[1]][hero_y[1]] = 4;
 	}
 	if (GetAsyncKeyState(0x53) & 0x8000 && position_x + 1 < High) { // s 下移
+		canvas[hero_x[0]][hero_y[0]] = 0;
+		canvas[hero_x[1]][hero_y[1]] = 0;
 		canvas[position_x][position_y] = 0;
+
 		position_x++;
+		hero_x[0] = position_x;// 左附机
+		hero_y[0] = position_y - 1;
+
+		hero_x[1] = position_x;// 右附机
+		hero_y[1] = position_y + 1;
+
 		canvas[position_x][position_y] = 1;
+		canvas[hero_x[0]][hero_y[0]] = 4;
+		canvas[hero_x[1]][hero_y[1]] = 4;
 	}
 	if (GetAsyncKeyState(' ') & 0x8000) { // 空格 发射子弹
 		canvas[position_x - 1][position_y] = 2;
+		canvas[hero_x[0] - 1][hero_y[0]] = 2;
+		canvas[hero_x[1] - 1][hero_y[1]] = 2;
 
 		TCHAR szStatus[80];
 		//mciSendString(TEXT("status NOWMUSIC position"), szStatus, 80, 0);
