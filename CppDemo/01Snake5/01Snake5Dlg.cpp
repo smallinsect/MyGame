ae47004298eea39ef5d3356cd34d833039739036
xuqiulin <513312514@qq.com>
2020-08-17 22:25:52 +0800

    MFC 贪吃蛇

--- CppDemo/01Snake5/01Snake5Dlg.cpp
+++ CppDemo/01Snake5/01Snake5Dlg.cpp
@@ -100,6 +100,45 @@ BOOL CMy01Snake5Dlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+	m_iWidth = 30;
+	m_iHeight = 20;
+
+	m_iNum = 4;
+	m_snake[0].x = 4; m_snake[0].y = 4;
+	m_snake[1].x = 4; m_snake[1].y = 5;
+	m_snake[2].x = 4; m_snake[2].y = 6;
+	m_snake[3].x = 4; m_snake[3].y = 7;
+	m_iDirect = M_Up;
+
+	m_food.x = rand() % m_iWidth;
+	m_food.y = rand() % m_iHeight;
+
+	// 加载红色位图
+	HBITMAP hbmp = (HBITMAP)LoadImage(NULL, TEXT("./res/red.bmp"), IMAGE_BITMAP, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	if (hbmp == NULL) {
+		AfxMessageBox(TEXT("加载图片失败"));
+	}
+
+	m_cbmRed.Attach(hbmp);
+	m_cbmRed.GetBitmap(&m_bmRed);
+
+	// 加载白色位图
+	hbmp = (HBITMAP)LoadImage(NULL, TEXT("./res/white.bmp"), IMAGE_BITMAP, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	if (hbmp == NULL) {
+		AfxMessageBox(TEXT("加载图片失败"));
+	}
+
+	m_cbmWhite.Attach(hbmp);
+	m_cbmWhite.GetBitmap(&m_bmWhite);
+
+	// 加载绿色位图
+	hbmp = (HBITMAP)LoadImage(NULL, TEXT("./res/green.bmp"), IMAGE_BITMAP, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	if (hbmp == NULL) {
+		AfxMessageBox(TEXT("加载图片失败"));
+	}
+
+	m_cbmGreen.Attach(hbmp);
+	m_cbmGreen.GetBitmap(&m_bmGreen);
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -123,27 +162,92 @@ void CMy01Snake5Dlg::OnSysCommand(UINT nID, LPARAM lParam)
 
 void CMy01Snake5Dlg::OnPaint()
 {
-	if (IsIconic())
-	{
-		CPaintDC dc(this); // 用于绘制的设备上下文
+	//if (IsIconic())
+	//{
+	//	CPaintDC dc(this); // 用于绘制的设备上下文
 
-		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+	//	SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// 使图标在工作区矩形中居中
-		int cxIcon = GetSystemMetrics(SM_CXICON);
-		int cyIcon = GetSystemMetrics(SM_CYICON);
-		CRect rect;
-		GetClientRect(&rect);
-		int x = (rect.Width() - cxIcon + 1) / 2;
-		int y = (rect.Height() - cyIcon + 1) / 2;
+	//	// 使图标在工作区矩形中居中
+	//	int cxIcon = GetSystemMetrics(SM_CXICON);
+	//	int cyIcon = GetSystemMetrics(SM_CYICON);
+	//	CRect rect;
+	//	GetClientRect(&rect);
+	//	int x = (rect.Width() - cxIcon + 1) / 2;
+	//	int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// 绘制图标
-		dc.DrawIcon(x, y, m_hIcon);
+	//	// 绘制图标
+	//	dc.DrawIcon(x, y, m_hIcon);
+	//}
+	//else
+	//{
+	//	CDialogEx::OnPaint();
+	//}
+	CDC* pDC = GetDC();
+
+	CRect rect;
+	GetClientRect(&rect);
+
+	CDC mDC;//内存DC
+	mDC.CreateCompatibleDC(pDC);
+
+	//创建内存位图
+	CBitmap mBmp;
+	mBmp.CreateCompatibleBitmap(pDC, rect.Width(), rect.Height());
+
+	mDC.SelectObject(&mBmp);//内存DC绑定内存位图
+	mBmp.DeleteObject();//删除内存位图
+
+	CDC memDC;
+	memDC.CreateCompatibleDC(pDC);
+
+	memDC.SelectObject(m_cbmWhite);
+	// 画背景
+	for (int x = 0; x < m_iWidth; x++) {
+		for (int y = 0; y < m_iHeight; y++) {
+			mDC.StretchBlt(x * m_bmWhite.bmWidth, y * m_bmWhite.bmHeight,
+				m_bmWhite.bmWidth, m_bmWhite.bmHeight,
+				&memDC,
+				0, 0,
+				m_bmWhite.bmWidth, m_bmWhite.bmHeight,
+				SRCCOPY);
+		}
 	}
-	else
-	{
-		CDialogEx::OnPaint();
+
+	memDC.SelectObject(m_cbmRed);
+	// 画蛇
+	for (int i = 0; i < m_iNum; i++) {
+		mDC.StretchBlt(m_snake[i].x * m_bmRed.bmWidth, m_snake[i].y * m_bmRed.bmHeight,
+			m_bmRed.bmWidth, m_bmRed.bmHeight,
+			&memDC,
+			0, 0,
+			m_bmRed.bmWidth, m_bmRed.bmHeight,
+			SRCCOPY);
 	}
+
+	memDC.SelectObject(m_cbmGreen);
+	//画食物
+	mDC.StretchBlt(m_food.x * m_bmGreen.bmWidth, m_food.y * m_bmGreen.bmHeight,
+		m_bmGreen.bmWidth, m_bmGreen.bmHeight,
+		&memDC,
+		0, 0,
+		m_bmGreen.bmWidth, m_bmGreen.bmHeight,
+		SRCCOPY);
+
+	memDC.DeleteDC();
+
+	pDC->StretchBlt(0, 0,
+		rect.Width(), rect.Height(),
+		&mDC,
+		0, 0,
+		rect.Width(), rect.Height(),
+		SRCCOPY);
+
+	mDC.DeleteDC();
+
+	ReleaseDC(pDC);
+
+
 }
 
 //当用户拖动最小化窗口时系统调用此函数取得光标
--- CppDemo/01Snake5/01Snake5Dlg.h
+++ CppDemo/01Snake5/01Snake5Dlg.h
@@ -31,4 +31,25 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+
+private:
+	enum DRICT {
+		M_Up = 0, M_Down, M_Left, M_Right
+	};
+	int m_iWidth;
+	int m_iHeight;
+
+	CBitmap m_cbmRed;
+	CBitmap m_cbmWhite;
+	CBitmap m_cbmGreen;
+
+	BITMAP m_bmRed;
+	BITMAP m_bmWhite;
+	BITMAP m_bmGreen;
+
+	int m_iNum;// 蛇的长度
+	int m_iDirect;// 蛇的方向
+	CPoint m_snake[100];
+
+	CPoint m_food;//食物的坐标
 };
