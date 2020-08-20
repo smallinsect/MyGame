1a59c5feef97181546c6f8b554b36b1718f65dd9
xuqiulin <513312514@qq.com>
2020-08-21 00:09:33 +0800

    俄罗斯方块

--- CppDemo/02Tetris1/main.cpp
+++ CppDemo/02Tetris1/main.cpp
@@ -6,11 +6,12 @@
 
 const int W = 10;// 屏幕宽度
 const int H = 20;// 屏幕高度
-int canvas[H][W];
+int canvas[H][W];// 游戏画布
 
 struct Point {
 	int x, y;
-} a[4], b[4];
+} a[4], b[4];// 下落的方块
+int tcanvas[H][W];//缓存的方块
 
 int figures[7][4] = {
 	1,3,5,7, // I
@@ -22,6 +23,10 @@ int figures[7][4] = {
 	2,3,4,5, // O
 };
 
+// 打表
+char symbol[] = " *";
+
+bool check();
 void gotoxy(SHORT x, SHORT y);
 void HideCursor();// 隐藏光标
 
@@ -47,12 +52,7 @@ int main(int argc, char* argv[]) {
 		// 将画布打印在控制台上
 		for (int i = 0; i < H; i++) {
 			for (int j = 0; j < W; j++) {
-				if (canvas[i][j] == 1) {// 蛇
-					printf("*");
-				}
-				else {// 空白
-					printf(" ");
-				}
+				printf("%c", symbol[canvas[i][j]]);
 			}
 			printf("|\n");
 		}
@@ -63,7 +63,7 @@ int main(int argc, char* argv[]) {
 
 		dx = 0;
 		rotate = false;
-		delay = 100;
+		delay = 300;
 		// 检测键盘输入
 		if (_kbhit()) {
 			char ch = _getch();
@@ -75,19 +75,28 @@ int main(int argc, char* argv[]) {
 
 		// 移动
 		for (int i = 0; i < 4; i++) {
-			canvas[a[i].y][a[i].x] = 0;// 重置原来位置的值
 			b[i] = a[i];
 			a[i].x += dx;
+		}
+		if (!check()) {
+			for (int i = 0; i < 4; i++) {
+				a[i] = b[i];
+			}
 		}
 		// 旋转
 		if (rotate) {
 			Point p = a[1];
 			for (int i = 0; i < 4; i++) {
-				canvas[a[i].y][a[i].x] = 0;// 重置原来位置的值
+				b[i] = a[i];
 				int x = a[i].y - p.y;
 				int y = a[i].x - p.x;
 				a[i].x = p.x - x;
 				a[i].y = p.y + y;
+			}
+			if (!check()) {
+				for (int i = 0; i < 4; i++) {
+					a[i] = b[i];
+				}
 			}
 		}
 
@@ -97,12 +106,45 @@ int main(int argc, char* argv[]) {
 			timer = nowTime;
 			// 下落
 			for (int i = 0; i < 4; i++) {
-				canvas[a[i].y][a[i].x] = 0;// 重置原来位置的值
 				b[i] = a[i];
 				a[i].y += 1;
 			}
+			// 下落最下面
+			if (!check()) {
+				for (int i = 0; i < 4; i++) {
+					tcanvas[b[i].y][b[i].x] = 1;
+				}
+				int n = rand() % 7;// 随机一种方块
+				for (int i = 0; i < 4; i++) {
+					a[i].x = figures[n][i] % 2;
+					a[i].y = figures[n][i] / 2;
+				}
+			}
 		}
 
+		// 消去满行
+		int k = H - 1;
+		for (int i = H - 1; i >= 0; i--) {
+			int cnt = 0;
+			// 统计一行的方块数量
+			for (int j = 0; j < W; j++) {
+				if (tcanvas[i][j] > 0) {
+					cnt++;
+				}
+				tcanvas[k][j] = tcanvas[i][j];
+			}
+			if (cnt < W) {
+				k--;
+			}
+		}
+
+		// 重置画布
+		memset(canvas, 0, sizeof(canvas));
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				canvas[i][j] = tcanvas[i][j];
+			}
+		}
 		for (int i = 0; i < 4; i++) {
 			canvas[a[i].y][a[i].x] = 1;
 		}
@@ -111,6 +153,17 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
+bool check() {
+	for (int i = 0; i < 4; i++) {
+		if (a[i].x < 0 || W <= a[i].x || a[i].y < 0 || H <= a[i].y) {
+			return false;
+		}
+		else if (tcanvas[a[i].y][a[i].x] > 0) {
+			return false;
+		}
+	}
+	return true;
+}
 
 // 设置光标位置
 void gotoxy(SHORT x, SHORT y) {
